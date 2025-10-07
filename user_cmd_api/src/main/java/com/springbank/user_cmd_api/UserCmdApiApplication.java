2f9ef484fd323f633c7e9cc148432e2e78751b01
Kamal Bouzekri <kamal.bouzekri@obi.de>
2025-10-07 10:42:47 +0200

    Init project

---
+++ user_cmd_api/src/main/java/com/springbank/user_cmd_api/UserCmdApiApplication.java
@@ -0,0 +1,13 @@
+package com.springbank.user_cmd_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCmdApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCmdApiApplication.class, args);
+    }
+
+}

50830ecffb58b7384935f2c5869924e8353e4a49
Kamal Bouzekri <kamal.bouzekri@obi.de>
2025-10-07 12:36:17 +0200

    Add mongoDB config

--- user_cmd_api/src/main/java/com/springbank/user_cmd_api/UserCmdApiApplication.java
+++ user_cmd_api/src/main/java/com/springbank/user_cmd_api/UserCmdApiApplication.java
@@ -1,9 +1,12 @@
 package com.springbank.user_cmd_api;
 
+import com.springbank.user_core.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({AxonConfig.class})
 public class UserCmdApiApplication {
 
     public static void main(String[] args) {
--- user_query_api/src/main/java/com/springbank/user_query_api/UserQueryApiApplication.java
+++ user_query_api/src/main/java/com/springbank/user_query_api/UserQueryApiApplication.java
@@ -1,13 +1,16 @@
 package com.springbank.user_query_api;
 
+import com.springbank.user_core.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({AxonConfig.class})
 public class UserQueryApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserQueryApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserQueryApiApplication.class, args);
+    }
 
 }
